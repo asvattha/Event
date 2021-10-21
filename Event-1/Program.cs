d128287bb6269cb36c6830d00b06fe3b0700d628
singhd1 <singhd1@sfasu.edu>
2021-10-21 12:15:29 -0500

    Add project files.

---
+++ Event-1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Event_1
+{
+
+    // Step 1: Create a delegate type
+    // Step 2: Use the delegate type to create an event, such that, all event handlers
+    // obeys the delegate type
+
+    public delegate void Notify();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weather w = new Weather();
+            w.HurricanIsComing += Citizen.evacuate;
+            w.HurricanIsComing += TVBroadcastAgency.broadcastNews;
+            w.HurricanIsComing += GovtAgency.provideFacility;
+            w.checkWeather();
+
+
+        }
+    }
+
+    class Citizen
+    {
+        public static void evacuate()
+        {
+            Console.WriteLine("Let's pack and leave");
+        }
+    }
+
+    class TVBroadcastAgency
+    {
+        public static void broadcastNews()
+        {
+            Console.WriteLine("Breaking News!! Hurricane is coming");
+        }
+    }
+
+    class GovtAgency
+    {
+        public static void provideFacility()
+        {
+            Console.WriteLine("Prepare for the hurricane");
+        }
+    }
+
+    class Weather {
+
+        public event Notify HurricanIsComing;
+
+        public void checkWeather()
+        {
+            if (true)
+            {
+                OnHurricaneIsComing(); // raising the event
+            }
+        }
+
+        public void OnHurricaneIsComing()
+        {
+            if (HurricanIsComing != null) // checking if there are subscribers to this event
+                HurricanIsComing.Invoke(); // invoke those event handlers/subscribers
+        }
+    }
+}
